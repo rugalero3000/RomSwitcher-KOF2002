7b5d17778dc76fd2363c6c00c3612fe8c9d265f0
Rugalero3000 <rugalero3k@gmail.com>
2025-08-28 00:26:54 -0500

    agregando formulario main

---
+++ R3K.KOF2002/R3K.KOF2002.RomSwitcher/FrmMain.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace R3K.KOF2002.RomSwitcher
+{
+  partial class FrmMain
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.btnActivateOriginalKof2002 = new System.Windows.Forms.Button();
+      this.btnActivateKof2002Green = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // btnActivateOriginalKof2002
+      // 
+      this.btnActivateOriginalKof2002.Location = new System.Drawing.Point(38, 84);
+      this.btnActivateOriginalKof2002.Name = "btnActivateOriginalKof2002";
+      this.btnActivateOriginalKof2002.Size = new System.Drawing.Size(172, 53);
+      this.btnActivateOriginalKof2002.TabIndex = 0;
+      this.btnActivateOriginalKof2002.Text = "Activar KOF Original";
+      this.btnActivateOriginalKof2002.UseVisualStyleBackColor = true;
+      this.btnActivateOriginalKof2002.Click += new System.EventHandler(this.btnActivateOriginalKof2002_Click);
+      // 
+      // btnActivateKof2002Green
+      // 
+      this.btnActivateKof2002Green.Location = new System.Drawing.Point(265, 84);
+      this.btnActivateKof2002Green.Name = "btnActivateKof2002Green";
+      this.btnActivateKof2002Green.Size = new System.Drawing.Size(172, 53);
+      this.btnActivateKof2002Green.TabIndex = 1;
+      this.btnActivateKof2002Green.Text = "Activar KOF Verde";
+      this.btnActivateKof2002Green.UseVisualStyleBackColor = true;
+      this.btnActivateKof2002Green.Click += new System.EventHandler(this.btnActivateKof2002Green_Click);
+      // 
+      // FrmMain
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(572, 193);
+      this.Controls.Add(this.btnActivateKof2002Green);
+      this.Controls.Add(this.btnActivateOriginalKof2002);
+      this.Name = "FrmMain";
+      this.Text = "KOF2002 Rom Switcher v1.0.0 by Rugalero3000";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button btnActivateOriginalKof2002;
+    private System.Windows.Forms.Button btnActivateKof2002Green;
+  }
+}
---
+++ R3K.KOF2002/R3K.KOF2002.RomSwitcher/FrmMain.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace R3K.KOF2002.RomSwitcher
+{
+  public partial class FrmMain : Form
+  {
+    public FrmMain()
+    {
+      InitializeComponent();
+    }
+
+    private void btnActivateOriginalKof2002_Click(object sender, EventArgs e)
+    {
+      ActivarRom(Util.GetBaseRomPath(), "kof2002_original", "kof2002.zip");
+      ActivarRom(Util.GetBaseRomPath(), "kf2k2pls_original", "kf2k2pls.zip");
+      //Refrescar();
+      MessageBox.Show("Activadas ROMs originales.");
+    }
+
+    private void btnActivateKof2002Green_Click(object sender, EventArgs e)
+    {
+      ActivarRom(Util.GetBaseRomPath(), "kof2002_verde", "kof2002.zip");
+      ActivarRom(Util.GetBaseRomPath(), "kf2k2pls_verde", "kf2k2pls.zip");
+      //Refrescar();
+      MessageBox.Show("Activadas ROMs hack (verde).");
+    }
+    private void ActivarRom(string rutaBaseRoms, string subCarpeta, string romFilename)
+    {
+      try
+      {
+        string pathOrigen = Path.Combine(rutaBaseRoms, subCarpeta, romFilename);
+        string pathDestino = Path.Combine(rutaBaseRoms, romFilename);
+
+        if (File.Exists(pathDestino))
+          File.Delete(pathDestino);
+
+        File.Copy(pathOrigen, pathDestino);
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show($"Error al activar la ROM: {ex.Message}");
+      }
+
+    }
+  }
+}
