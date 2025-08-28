0ed65aae0c07c3e3ecdc6cd3955d58fc25c26198
Rugalero3000 <rugalero3k@gmail.com>
2025-08-27 21:34:33 -0500

    avances

--- R3K.KOF2002/R3K.KOF2002.RomSwitcher/FrmLoader.cs
+++ R3K.KOF2002/R3K.KOF2002.RomSwitcher/FrmLoader.cs
@@ -5,9 +5,11 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace R3K.KOF2002.RomSwitcher
 {
@@ -20,6 +22,21 @@ namespace R3K.KOF2002.RomSwitcher
     private string kof2002PlusRomNameDefault = "kf2k2pls.zip";
     private string kof2002VerdeRomNameDefault = "kof2002.zip";
     private string kof2002PlusVerdeRomNameDefault = "kf2k2pls_verde.zip";
+
+    private const string kof2002_original_hash = "870204ac21027c010de7a70da00da2202bb710a16f7c01b9b11ab680fe5c2c51";
+    private const string kf2k2pls_original_hash = "9a7b976a47e4153a6c10be679b445b6b628fafc53b0c264947d48d0240bd90ea";
+
+    private const string kof2002_verde_hash = "ce6f101a2f1d33c56d7f4a44b4a5e168894ba37a897aace43dabbc60dae87685";
+    private const string kf2k2pls_verde_hash = "f9fda0ab66f0d63647f80985ca9363e0dc6582dfef3edc58ad238610502df8de";
+
+    private Dictionary<string, string> romHashes = new Dictionary<string, string>
+        {
+            { "kof2002_original", kof2002_original_hash },
+            { "kf2k2pls_original", kf2k2pls_original_hash },
+            { "kof2002_verde", kof2002_verde_hash },
+            { "kf2k2pls_verde", kf2k2pls_verde_hash }
+        };
+    List<ROM> romList = new List<ROM>();
     public FrmLoader()
     {
       InitializeComponent();
@@ -46,14 +63,57 @@ namespace R3K.KOF2002.RomSwitcher
       string kof2002VerdePath = Path.Combine(directoryRomsVerde, kof2002VerdeRomNameDefault);
       if (File.Exists(kof2002VerdePath))
       {
-        Console.WriteLine("KOF 2002 Verde encontrado.");
+        Console.WriteLine("KOF 2002 Verde encontrado. No hacer nada");
         return;
       }
       string kof2002VerdePathDefault = Path.Combine(directoryRomsDefault, kof2002VerdeRomNameDefault);
       if (File.Exists(kof2002VerdePathDefault))
       {
-        Console.WriteLine("KOF 2002 Verde encontrado.");
+        Console.WriteLine("KOF 2002 Verde encontrado. Mover a carpeta kof2002_verde");
         return;
+      }
+
+      if (romList.Count == 0)
+      {
+        var archivos = Directory.GetFiles(directoryRomsDefault, "*.zip");
+
+        foreach (var archivo in archivos)
+        {
+          string hash = CalcularSHA256(archivo);
+
+          foreach (var kvp in romHashes)
+          {
+            ROM rom = null;
+            if (hash.Equals(kvp.Value, StringComparison.OrdinalIgnoreCase))
+            {
+              //Console.WriteLine($"Encontrado: {kvp.Key} en archivo {Path.GetFileName(archivo)}");
+              rom = new ROM()
+              {
+                Tag = kvp.Key,
+                Hash = hash,
+                FileName = Path.GetFileName(archivo)
+              };
+              romList.Add(rom);
+            }
+          }
+        }
+      }
+      var romKofVerdeItem = romList.Where(x => x.Hash == kof2002_verde_hash).FirstOrDefault();
+      if(romKofVerdeItem != null)
+      {
+        Console.WriteLine("KOF 2002 Verde encontrado. Mover a carpeta kof2002_verde");
+        return;
+      }
+
+      //TODO: no encontrado, especificar ruta base ROM
+    }
+    private string CalcularSHA256(string filePath)
+    {
+      using (var sha256 = SHA256.Create())
+      using (var stream = File.OpenRead(filePath))
+      {
+        byte[] hashBytes = sha256.ComputeHash(stream);
+        return BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
       }
     }
     private void VerificarKof2002PlusVerde()
---
+++ R3K.KOF2002/R3K.KOF2002.RomSwitcher/ROM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R3K.KOF2002.RomSwitcher
+{
+  internal class ROM
+  {
+    public string Tag { get; set; }
+    public string Hash { get; set; }
+    public string FileName { get; set; }
+  }
+}
